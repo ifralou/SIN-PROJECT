f8b617b2d88b668b6b79836f359d887e76990775
fraloily <fraloily@fel.cvut.cz>
2022-10-02 13:25:35 +0200

    Sem1 commit.

---
+++ src/main/java/cz/fraloily/implementationpartsin/ImplementationPartSinApplication.java
@@ -0,0 +1,13 @@
+package cz.fraloily.implementationpartsin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementationPartSinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplementationPartSinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/fraloily/implementationpartsin/ImplementationPartSinApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.fraloily.implementationpartsin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementationPartSinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
